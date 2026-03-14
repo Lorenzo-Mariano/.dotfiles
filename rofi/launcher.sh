d30a3289a4d7c0e72b0e6da2225766d6752b47c0
Lorenzo-Mariano <hhumminah@gmail.com>
2026-03-14 10:55:16 +0800

    add famine theme

--- rofi/launcher.sh
+++ rofi/launcher.sh
@@ -9,11 +9,16 @@
 # launcher script. Happy ricing!
 
 dir="$HOME/.config/rofi/"
+# we need like some sort of red theme ngl
+# also a pinkish theme like bocchi but with more aura
+# need one that goes well wit da robin pape
 # theme='calli-pink'
 # theme='yae-miko'
 # theme='bocchi-light'
 # theme='bocchi-dark'
-theme='monochrome'
+# theme='monochrome'
+theme='famine'
+# theme='gruvbox'
 
 ## Run
 rofi \
